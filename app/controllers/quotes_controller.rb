407e4fef1b56fcd792a0b5cd16fa92cd714e4ca1
nikolatriki <kjofte@yahoo.com>
2022-07-18 21:45:55 +0200

    add controller quotes and install simple_form gem

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class QuotesController < ApplicationController
+  before_action :set_quote, only: %i[show edit update destroy]
+
+  def index
+    @quotes = Quote.all
+  end
+
+  def show; end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_path, notice: 'Quote was successfuly created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: 'Quote successfuly updated!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @quote.destroy
+    redirect_to quotes_path, notice: 'quote was deleted!'
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
+end
